128e66828e21453127cbf95534ae4e93fba96e04
RiccardoBusetti <riccardob36@gmail.com>
2018-10-08 16:24:54 +0200

    Added routing for login.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
 import './App.css';
+import { Navigation } from './components/Navigation';
 
 class App extends Component {
   constructor(props) {
     super(props);
   }
 
-  componentWillReceiveProps(nextProps) {
-  }
+  componentWillReceiveProps(nextProps) {}
 
   render() {
-    return (<div></div>);
+    return (
+      <div>
+        <Route path="/login" component={Navigation} />
+      </div>
+    );
   }
 }
 
