afa385a358adbbc1b3f54b91f2eae081cba5058e
RiccardoBusetti <riccardob36@gmail.com>
2018-10-23 18:27:41 +0200

    Added left drawer.

--- src/components/HomeAppBar/HomeAppBar.jsx
+++ src/components/HomeAppBar/HomeAppBar.jsx
@@ -1,23 +1,32 @@
 import AppBar from '@material-ui/core/AppBar';
+import Avatar from '@material-ui/core/Avatar';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import { withStyles } from '@material-ui/core/styles';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import MenuIcon from '@material-ui/icons/Menu';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { attemptLogout } from '../../actions/auth';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
 import { getRandomColor } from '../../utils/colorUtils';
+import CodeIcon from '@material-ui/icons/Code';
+import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import FaceIcon from '@material-ui/icons/Face';
 
 const TABS_VALUES = {
   DEVELOPER_VALUE: 0,
@@ -30,22 +39,26 @@ const tabs = [
   {
     label: 'Programmatore',
     disabled: false,
-    value: TABS_VALUES.DEVELOPER_VALUE
+    value: TABS_VALUES.DEVELOPER_VALUE,
+    icon: <CodeIcon />
   },
   {
     label: 'Referente area tecnica',
     disabled: false,
-    value: TABS_VALUES.REFERENT_VALUE
+    value: TABS_VALUES.REFERENT_VALUE,
+    icon: <RecordVoiceOverIcon />
   },
   {
     label: 'Responsabile ufficio revisioni',
     disabled: false,
-    value: TABS_VALUES.MANAGER_VALUE
+    value: TABS_VALUES.MANAGER_VALUE,
+    icon: <AttachMoneyIcon />
   },
   {
     label: 'Cliente',
     disabled: false,
-    value: TABS_VALUES.CUSTOMER_VALUE
+    value: TABS_VALUES.CUSTOMER_VALUE,
+    icon: <FaceIcon />
   }
 ];
 
@@ -56,9 +69,15 @@ const styles = {
   grow: {
     flexGrow: 1
   },
+  hamburgerMenu: {
+    marginRight: 16
+  },
   avatar: {
     margin: -8,
     backgroundColor: getRandomColor()
+  },
+  drawerItems: {
+    width: 'auto'
   }
 };
 
@@ -73,9 +92,17 @@ class HomeAppBar extends Component {
 
     this.state = {
       selectedValue: 0,
-      anchorEl: null
+      anchorEl: null,
+      isDrawerOpen: false
     };
 
+    this.renderMenu = this.renderMenu.bind(this);
+    this.renderTabs = this.renderTabs.bind(this);
+    this.renderTabsViews = this.renderTabsViews.bind(this);
+    this.renderDrawer = this.renderDrawer.bind(this);
+
+    this.openDrawer = this.openDrawer.bind(this);
+    this.closeDrawer = this.closeDrawer.bind(this);
     this.onTabSelectionChanged = this.onTabSelectionChanged.bind(this);
     this.onMenuButtonClicked = this.onMenuButtonClicked.bind(this);
     this.onMenuClose = this.onMenuClose.bind(this);
@@ -89,6 +116,16 @@ class HomeAppBar extends Component {
       <div className={classes.root}>
         <AppBar position="static">
           <Toolbar>
+            <Hidden mdUp>
+              <IconButton
+                className={classes.hamburgerMenu}
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.openDrawer}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Hidden>
             <Typography variant="h6" color="inherit" className={classes.grow}>
               Authorization manager
             </Typography>
@@ -102,52 +139,68 @@ class HomeAppBar extends Component {
             >
               <MoreVertIcon />
             </IconButton>
-            <Menu
-              id="toolbar-menu"
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={this.onMenuClose}
-            >
-              <MenuItem onClick={() => console.log('Profile clicked')}>
-                <ListItemIcon>
-                  <Avatar className={classes.avatar}>A</Avatar>
-                </ListItemIcon>
-                <ListItemText
-                  classes={{ primary: classes.primary }}
-                  inset
-                  primary="Admin"
-                />
-              </MenuItem>
-              <MenuItem onClick={this.onLogoutButtonClicked}>
-                <ListItemIcon>
-                  <ExitToAppIcon />
-                </ListItemIcon>
-                <ListItemText
-                  classes={{ primary: classes.primary }}
-                  inset
-                  primary="Logout"
-                />
-              </MenuItem>
-            </Menu>
+            {this.renderMenu()}
           </Toolbar>
-          <Tabs
-            value={selectedValue}
-            onChange={this.onTabSelectionChanged}
-            scrollable
-            scrollButtons="auto"
-          >
-            {tabs.map((tab, index) => (
-              <Tab
-                key={index}
-                value={tab.value}
-                label={tab.label}
-                disabled={tab.disabled}
-              />
-            ))}
-          </Tabs>
+          <Hidden smDown>{this.renderTabs()}</Hidden>
         </AppBar>
         {this.renderTabsViews(selectedValue)}
+        {this.renderDrawer()}
       </div>
+    );
+  }
+
+  renderMenu() {
+    const { classes } = this.props;
+    const { anchorEl } = this.state;
+    return (
+      <Menu
+        id="toolbar-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={this.onMenuClose}
+      >
+        <MenuItem onClick={() => console.log('Profile clicked')}>
+          <ListItemIcon>
+            <Avatar className={classes.avatar}>A</Avatar>
+          </ListItemIcon>
+          <ListItemText
+            classes={{ primary: classes.primary }}
+            inset
+            primary="Admin"
+          />
+        </MenuItem>
+        <MenuItem onClick={this.onLogoutButtonClicked}>
+          <ListItemIcon>
+            <ExitToAppIcon />
+          </ListItemIcon>
+          <ListItemText
+            classes={{ primary: classes.primary }}
+            inset
+            primary="Logout"
+          />
+        </MenuItem>
+      </Menu>
+    );
+  }
+
+  renderTabs() {
+    const { selectedValue } = this.state;
+    return (
+      <Tabs
+        value={selectedValue}
+        onChange={this.onTabSelectionChanged}
+        scrollable
+        scrollButtons="auto"
+      >
+        {tabs.map((tab, index) => (
+          <Tab
+            key={index}
+            value={tab.value}
+            label={tab.label}
+            disabled={tab.disabled}
+          />
+        ))}
+      </Tabs>
     );
   }
 
@@ -164,6 +217,40 @@ class HomeAppBar extends Component {
       default:
         return <h1>Programmatore</h1>;
     }
+  }
+
+  renderDrawer() {
+    const { classes } = this.props;
+    const { isDrawerOpen } = this.state;
+    return (
+      <Drawer open={isDrawerOpen} onClose={this.closeDrawer}>
+        <div
+          tabIndex={0}
+          role="button"
+          onClick={this.closeDrawer}
+          onKeyDown={this.closeDrawer}
+        >
+          <List className={classes.drawerItems} component="nav">
+            {tabs.map((tab, index) => (
+              <ListItem key={index} button>
+                <ListItemIcon>
+                  {tab.icon}
+                </ListItemIcon>
+                <ListItemText primary={tab.label} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
+    );
+  }
+
+  openDrawer() {
+    this.setState({ isDrawerOpen: true });
+  }
+
+  closeDrawer() {
+    this.setState({ isDrawerOpen: false });
   }
 
   onTabSelectionChanged(event, value) {
