6050c51a1ef224c301db09d65282ef497b7d9916
RiccardoBusetti <riccardob36@gmail.com>
2018-11-05 19:17:56 +0100

    Improved commits table and fixed some layouts.

--- src/components/CommitsTable/CommitsTable.jsx
+++ src/components/CommitsTable/CommitsTable.jsx
@@ -5,52 +5,90 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
-const styles = {};
+const styles = {
+  root: {
+    flexGrow: 1,
+    width: '100%'
+  }
+};
 
 /**
  * @class
- * This class is responsible of displaying a table 
+ * This class is responsible of displaying a table
  * with specific data.
  */
 class CommitsTable extends Component {
   constructor(props) {
     super(props);
+
+    this.renderTableToolbar = this.renderTableToolbar.bind(this);
+    this.renderTableHeader = this.renderTableHeader.bind(this);
+    this.renderTableBody = this.renderTableBody.bind(this);
   }
 
   render() {
-    const { tableHeaderLabels, tableData } = this.props;
+    const { classes } = this.props;
     return (
-      <Paper>
+      <Paper className={classes.root}>
+        {this.renderTableToolbar()}
         <Table>
-          <TableHead>
-            <TableRow>
-              {tableHeaderLabels.map(headerValue => (
-                <TableCell>{headerValue}</TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {tableData.map(rowValue => {
-              return (
-                <TableRow>
-                  {rowValue.map(columnValue => (
-                    <TableCell>{columnValue}</TableCell>
-                  ))}
-                </TableRow>
-              );
-            })}
-          </TableBody>
+          {this.renderTableHeader()}
+          {this.renderTableBody()}
         </Table>
       </Paper>
+    );
+  }
+
+  renderTableToolbar() {
+    const { tableToolbarTitle } = this.props;
+    return (
+      <Toolbar>
+        <Typography variant="h5">
+          {tableToolbarTitle}
+        </Typography>
+      </Toolbar>
+    );
+  }
+
+  renderTableHeader() {
+    const { tableHeaderLabels } = this.props;
+    return (
+      <TableHead>
+        <TableRow>
+          {tableHeaderLabels.map(headerValue => (
+            <TableCell>{headerValue}</TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  }
+
+  renderTableBody() {
+    const { tableData } = this.props;
+    return (
+      <TableBody>
+        {tableData.map(rowValue => {
+          return (
+            <TableRow>
+              {rowValue.map(columnValue => (
+                <TableCell>{columnValue}</TableCell>
+              ))}
+            </TableRow>
+          );
+        })}
+      </TableBody>
     );
   }
 }
 
 CommitsTable.propTypes = {
   classes: PropTypes.object.isRequired,
+  tableToolbarTitle: PropTypes.string.isRequired,
   tableHeaderLabels: PropTypes.array.isRequired,
   tableData: PropTypes.array.isRequired
 };
--- src/views/ProgrammerView/ProgrammerView.jsx
+++ src/views/ProgrammerView/ProgrammerView.jsx
@@ -1,13 +1,61 @@
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import CommitsTable from '../../components/CommitsTable/CommitsTable';
 
-export default class ProgrammerView extends Component {
-    constructor(props) {
-        super(props);
-    }
+const COMMITS_TABLE_HEADER = ['ID', 'Descrizione', 'Data', 'Autore'];
 
-    render() {
-        return (
-            <div></div>
-        )
-    }
+const styles = {
+  root: {
+    flexGrow: 1,
+    padding: 16
+  }
+};
+
+/**
+ * @class
+ * This class is resposible of displaying the proper
+ * components for the programmer view.
+ */
+class ProgrammerView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Grid container className={classes.root} spacing={16}>
+        <Grid item xs={12}>
+          <Grid container justify="center">
+            <CommitsTable
+              tableToolbarTitle="Lista commit"
+              tableHeaderLabels={COMMITS_TABLE_HEADER}
+              tableData={[
+                [
+                  '3rns3dse',
+                  'Improved search algorithm.',
+                  '2018/08/09',
+                  'Riccardo Busetti'
+                ],
+                [
+                  '45efdfhsa',
+                  'Fixed startup issues.',
+                  '2018/08/01',
+                  'Francesco Saltori'
+                ]
+              ]}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
 }
+
+ProgrammerView.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ProgrammerView);
