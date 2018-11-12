94cab26a0c67a88895985f296b4ad27dd8f4ec44
Fs00 <francescosaltori@gmail.com>
2018-11-12 18:47:16 +0100

    Minor fixes after merging with devel Still a lot of todos...

--- src/components/LoginCard/LoginCard.jsx
+++ src/components/LoginCard/LoginCard.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -16,31 +14,21 @@ import { Grid } from '@material-ui/core';
 const styles = {
   card: {
     minWidth: 450,
-    marginTop: 200,
-    
-
+    marginTop: 200
   },
   textField: {
-    width: 390,
+    width: 390
   },
-
-  button:{
-    marginTop: 16,
-  },
-
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+  button: {
+    marginTop: 16
   },
   title: {
-    fontSize: 14,
+    fontSize: 14
   },
   pos: {
-    marginBottom: 12,
-  },
+    marginBottom: 12
+  }
 };
-
 
 class LoginCard extends Component {
   constructor(props) {
@@ -54,56 +42,55 @@ class LoginCard extends Component {
 
   render() {
     const { classes } = this.props;
-    const bull = <span className={classes.bullet}>•</span>;
     return (
       <div>
-          <Grid
-            spacing ={16}
-            xs = {12}
-            container
-            justify="center"
-            alignItems="center" 
-          >
-            <Card className={classes.card} >
-              <CardContent>
-                <Typography gutterBottom variant="h4" component="h2">
-                  Gesbank Evolution 
-                </Typography>
-                <Typography gutterBottom variant="h7"  color="textSecondary">
-                  Benvenuto in Authorization Manager
-                </Typography>
-                <TextField
-                  id="standard-textarea1"
-                  label="Username"
-                  placeholder=""
-                  multiline
-                  className={classes.textField}
-                  margin="normal"
-                  
-                />
-                <br/>
-                <TextField
-                  id="standard-textarea2"
-                  label="Password"
-                  placeholder=""
-                  multiline
-                  className={classes.textField}
-                  margin="normal"
-                />
-                <Grid
-                  
-                  container
-                  justify="flex-end"
-                  alignItems="flex-start"
+        <Grid
+          spacing={16}
+          xs={12} // FIXME it shouldn't be used here, but it is bugged without it!
+          container
+          justify="center"
+          alignItems="center"
+        >
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography gutterBottom variant="h4" component="h2">
+                Gesbank Evolution
+              </Typography>
+              <Typography gutterBottom variant="subtitle1" color="textSecondary">
+                Benvenuto in Authorization Manager
+              </Typography>
+              <TextField
+                id="username-field"
+                label="Username"
+                placeholder=""
+                multiline // FIXME bugged without it!
+                className={classes.textField}
+                margin="normal"
+              />
+              <br />
+              <TextField
+                id="password-field"
+                label="Password"
+                placeholder=""
+                multiline // FIXME bugged without it!
+                className={classes.textField}
+                type="password"
+                margin="normal"
+              />
+              <Grid container justify="flex-end" alignItems="flex-start">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  className={classes.button}
+                  onClick={() => this.props.attemptLogin(this.state.username, this.state.password)}
                 >
-                  <Button variant="contained" color="primary" size = "large" className={classes.button}>
-                    Login 
-                  </Button>
-                </Grid>
-                
-              </CardContent>
-            </Card>
-          </Grid>
+                  Login
+                </Button>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
       </div>
     );
   }
@@ -113,8 +100,8 @@ const mapStateToProps = state => {
   return {
     isAttemptingLogin: state.auth.isAttemptingLogin,
     loginErrorMessage: state.auth.loginErrorMessage
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators(
@@ -123,10 +110,11 @@ const mapDispatchToProps = dispatch => {
     },
     dispatch
   );
-}
+};
 
-
-export default withStyles(styles) (connect (
-  mapStateToProps,
-  mapDispatchToProps
-)(LoginCard));
+export default withStyles(styles)(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(LoginCard)
+);
