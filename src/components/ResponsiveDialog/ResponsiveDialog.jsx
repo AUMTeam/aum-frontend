172f5db4458919e2ff09d805225719a780680bb4
Fs00 <francescosaltori@gmail.com>
2019-04-21 18:18:28 +0200

    Implement responsive dialog component

---
+++ src/components/ResponsiveDialog/ResponsiveDialog.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import withMobileDialog from '@material-ui/core/withMobileDialog';
+
+/**
+ * Dialog that resizes dynamically to fit screen size up to medium breakpoint (960px).
+ * On mobile screens it becomes fullscreen.
+ * Any given props will be passed down to the Dialog MaterialUI component.
+ */
+class ResponsiveDialog extends React.Component {
+  render() {
+    return (
+      <Dialog fullWidth maxWidth={'md'} disableBackdropClick {...this.props}>
+        {this.props.children}
+      </Dialog>
+    );
+  }
+}
+
+ResponsiveDialog.displayName = 'ResponsiveDialog';
+
+export default withMobileDialog({ breakpoint: 'xs' })(ResponsiveDialog);
