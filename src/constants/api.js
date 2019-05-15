1e2b846d21ba563b4b4215399b1d00d4a8ebfded
Francesco Saltori <francescosaltori@gmail.com>
2019-05-15 12:15:41 +0200

    Tweak timeout and update checking timings

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,8 @@
 export let API_ENDPOINT_URL = 'https://aum.altervista.org/main.php';
 export const TOKEN_LOCALSTORAGE_KEY = 'token';
 
-export const REQUEST_TIMEOUT_MS = 7000;
-export const LIST_AUTO_UPDATE_INTERVAL_MS = 10000;
+export const REQUEST_TIMEOUT_MS = 8000;
+export const LIST_AUTO_UPDATE_INTERVAL_MS = 5000;
 export const LIST_ELEMENTS_PER_PAGE = 20;
 
 export const SEARCH_DEBOUNCE_DELAY_MS = 250;
