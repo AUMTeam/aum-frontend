0852ebc8a1cd5eeb986b6017b8c6b9ac3d481367
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-04 16:17:55 +0000

    Deploy AUMTeam/aum-frontend to github.com/AUMTeam/aum-frontend.git:gh-pages

---
+++ precache-manifest.9125a4f1027d514c34a7c5bf85afa368.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a351722994a4b665570d6f816415fcb6",
+    "url": "/aum-frontend/index.html"
+  },
+  {
+    "revision": "410cc6fec1baae9c5dfe",
+    "url": "/aum-frontend/static/js/2.f57883ea.chunk.js"
+  },
+  {
+    "revision": "9d6a0f815aefa59248d7",
+    "url": "/aum-frontend/static/js/main.5b6546e3.chunk.js"
+  },
+  {
+    "revision": "1f13eba79cb1f8da913e",
+    "url": "/aum-frontend/static/js/runtime~main.a39234c4.js"
+  },
+  {
+    "revision": "d8e4f43cc76df219bc68e985e3a13586",
+    "url": "/aum-frontend/static/media/gesbank_logo.d8e4f43c.png"
+  },
+  {
+    "revision": "c6af17aceb9a6a6eb8f6a10fccb9bd45",
+    "url": "/aum-frontend/static/media/ibt_logo.c6af17ac.png"
+  },
+  {
+    "revision": "391e5c2b1d263f7303ec08ae13a0f437",
+    "url": "/aum-frontend/static/media/sad_face_grey.391e5c2b.png"
+  }
+]);
